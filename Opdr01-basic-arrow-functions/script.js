2fa320c8eb0ae96ed128f3aed7728fafd1667c3d
Juliazijd <68395721+Juliazijd@users.noreply.github.com>
2021-03-16 11:56:17 +0100

    Add files via upload

---
+++ Opdr01-basic-arrow-functions/script.js
@@ -0,0 +1,12 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+const fivePlusSeven = () => 5 + 7;
+const myFunction = (a, b) => a + b;
+const addFive = a => a + 5;
+const createObject = () => ({greeting: "hoi"});
+
+ikRockArrowFunctions()
+fivePlusSeven();
+myFunction(1, 5);
+addFive(5);
+createObject();
